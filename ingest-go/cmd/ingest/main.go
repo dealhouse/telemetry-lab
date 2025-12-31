1113d6247bbc3fdf35408b60463d755a11bf1228
dealhouse <de.juan1@live.com>
2025-12-30 21:22:21 -0500

    formatting

--- ingest-go/cmd/ingest/main.go
+++ ingest-go/cmd/ingest/main.go
@@ -110,20 +110,21 @@ func main() {
 	r.Get("/docs", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
-		_, _ = w.Write([]byte(`<!doctype html>
-<html>
-<head>
-  <title>Telemetry Ingest API Docs</title>
-  <link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
-</head>
-<body>
-<div id="swagger-ui"></div>
-<script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
-<script>
-  window.ui = SwaggerUIBundle({ url: "/openapi.yaml", dom_id: "#swagger-ui" });
-</script>
-</body>
-</html>`))
+		_, _ = w.Write([]byte(
+			`<!doctype html>
+			<html>
+			<head>
+			<title>Telemetry Ingest API Docs</title>
+			<link rel="stylesheet" href="https://unpkg.com/swagger-ui-dist@5/swagger-ui.css">
+			</head>
+			<body>
+			<div id="swagger-ui"></div>
+			<script src="https://unpkg.com/swagger-ui-dist@5/swagger-ui-bundle.js"></script>
+			<script>
+			window.ui = SwaggerUIBundle({ url: "/openapi.yaml", dom_id: "#swagger-ui" });
+			</script>
+			</body>
+			</html>`))
 	})
 
 	log.Printf("ingest listening on %s (db=%s)", addr, dbPath)
